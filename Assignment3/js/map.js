8b847b134ad970ec8ec78d45d5080d33ebb3c0fd
ChristianLambert <christianlambert412@gmail.com>
2026-02-16 16:38:27 +0100

    lab done(?)

--- Assignment3/js/map.js
+++ Assignment3/js/map.js
@@ -11,22 +11,32 @@ function worldMap(data) {
     /**
      * Task 14 - Create a leaflet map and put center to 10,15 with zoom scale of 1
      */
-
+    var leaflet_map = L.map("mapid").setView([10, 15], 1);
     /**
      * Task 15 - Get the tileLayer from the link at the bottom of this file
      * and add it to the map created above.
     */
-
+     L.tileLayer(map_link(), {
+    maxZoom: 19,
+    }).addTo(leaflet_map);
     /**
      * Task 16 - Create an svg call on top of the leaflet map.
      * Also append a g tag on this svg tag and add class leaflet-zoom-hide.
      * This g tag will be needed later.
      */
 
+    var svg_map = d3.select(leaflet_map.getPanes().overlayPane).append("svg");
+    var g = svg_map.append("g").attr("class", "leaflet-zoom-hide");
+
     /**
      * Task 17 - Create a function that projects lat/lng points on the map.
      * Use latLngToLayerPoint, remember which goes where.
      */
+
+    function projectPointsOnMap(x, y) {
+    var point = leaflet_map.latLngToLayerPoint(new L.latLng(y, x));
+    return this.stream.point(point.x, point.y);
+  }
 
     /**
      * Task 18 - Now we need to transform all to the specific projection
@@ -35,6 +45,10 @@ function worldMap(data) {
      * Create another variable names d3geoPath to project this transformation to it.
      */
     //Transforming to the specific projection
+
+      var transform = d3.geoTransform({ point: projectPointsOnMap });
+      var d3path = d3.geoPath().projection(transform);
+
 
     // similar to projectPoint this function converts lat/long to
     //svg coordinates except that it accepts a point from our
@@ -56,20 +70,33 @@ function worldMap(data) {
      */
     //features for the points
 
+      var feature = g
+    .selectAll("circle")
+    .data(data.features)
+    .enter()
+    .append("circle")
+    .attr("class", "mapcircle")
+    .attr("r", 5)
+    .attr("fill", "red")
+    .style("opacity", 0.4);
+
     /**
      * Task 20 - Call the plot function with feature variable
      * not integers needed.
      */
 
+
+    var points = new Points();
+    points.plot(feature, 1, 1);
     //Redraw the dots each time we interact with the map
     //Remove comment tags when done with task 20
-    //leaflet_map.on("moveend", reset);
-    //reset();
+    leaflet_map.on("moveend", reset);
+    reset();
 
     //Mouseover
     //Remove comment tags when done with task 20
-    //mouseOver(feature);
-    //mouseOut(feature);
+    mouseOver(feature);
+    mouseOut(feature);
 
     //Mouse over function
     function mouseOver(feature){
