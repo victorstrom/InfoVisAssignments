d565ad7d7ff2c0b4875281248fe7e355a31ae140
Victor Ström <carlvictorstrom@gmail.com>
2026-02-18 10:48:11 +0100

    last thing

--- Assignment3/js/fpc.js
+++ Assignment3/js/fpc.js
@@ -292,7 +292,7 @@ small_points = dots.selectAll("dot")
             /**
              * Remove comment for updating dots on the map.
              */
-            //curr_points_view = world_map.change_map_points(curr_view_erth)
+            curr_points_view = world_map.change_map_points(curr_view_erth)
         }
     }
 
